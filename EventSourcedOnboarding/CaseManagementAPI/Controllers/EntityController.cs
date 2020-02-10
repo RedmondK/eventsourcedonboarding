b3cba43a70b1ab4465435caaff16943be922d697
Keith Redmond <Keith.Redmond@fenergo.com>
2020-02-10 10:42:25 +0000

    Added an entity controller to hold entity command actions

---
+++ EventSourcedOnboarding/CaseManagementAPI/Controllers/EntityController.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Commands;
+using EventStore.ClientAPI;
+using EventStoreFramework;
+using EventStoreFramework.Command;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CaseManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EntityController
+    {
+        private readonly ILogger<EntityController> _logger;
+        private readonly Dispatcher _commandDispatcher;
+
+        public EntityController(ILogger<EntityController> logger)
+        {
+            _logger = logger;
+            _commandDispatcher = SetupDispatcher().GetAwaiter().GetResult();
+        }
+
+        private static async Task<Dispatcher> SetupDispatcher()
+        {
+            var eventStoreConnection = EventStoreConnection.Create(
+                ConnectionSettings.Default,
+                new IPEndPoint(IPAddress.Loopback, 1113));
+
+            await eventStoreConnection.ConnectAsync();
+
+            var repository = new EventStoreRepository(eventStoreConnection);
+
+            var commandHandlerMap = new CommandHandlerMap(new Handlers(repository));
+
+            return new Dispatcher(commandHandlerMap);
+        }
+    }
+}
